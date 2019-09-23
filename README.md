6ab60fb5c61a927fb532f558ad981bf714ea0ccc
张添龙 <tzhang2@xiaohongshu.com>
2019-09-23 14:52:28 +0800

    up

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1. zset的使用，实现排行榜功能
 2. 自定义validation
 3. OptionalInt使用
+4. reduce的两种使用姿势
--- src/test/java/com/example/boottest/OptionalIntTest.java
+++ src/test/java/com/example/boottest/OptionalIntTest.java
@@ -1,7 +1,9 @@
 package com.example.boottest;
 
+import org.assertj.core.util.Lists;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.OptionalInt;
 
 public class OptionalIntTest {
@@ -16,5 +18,16 @@ public class OptionalIntTest {
         System.out.println("======:" + op.orElse(111));
         System.out.println("xxxx: " + op.orElseGet(() -> 333));
         op.orElseThrow(NullPointerException::new);
+    }
+
+    @Test
+    public void testReduce() {
+        List<Integer> list = Lists.newArrayList();
+        int sum = list.stream().reduce(Integer::sum).orElse(10);
+        System.out.println("====sum:" + sum);
+
+        List<Long> listx = Lists.newArrayList(1L,2L,3L);
+        long sumx = listx.stream().reduce(10L, Long::sum);
+        System.out.println("====sumx:" + sumx);
     }
 }
